9c88ba3800097ea06866140566a19a06ea71ae1c
Lucas Koch <lucasgabrielkoch1997@gmail.com>
2024-04-10 14:00:34 -0300

    Se modifico el archivo streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,18 +2,7 @@ import streamlit as st
 import pickle
 import numpy as np
 
-"""
-# Cargar el modelo entrenado
-modelo_path = 'modelo_entrenado.pkl'
-with open(modelo_path, 'rb') as archivo:
-    modelo = pickle.load(archivo)
 
-# Función para predecir la probabilidad de abandono del carrito
-def predecir_abandono_carrito(datos_cliente):
-    X_cliente = np.array(datos_cliente).reshape(1, -1)
-    probabilidad_abandono = modelo.predict_proba(X_cliente)[:, 1]
-    return probabilidad_abandono[0]
-"""
 
 # Interfaz de usuario de la aplicación
 st.title('Predicción de Abandono de Carrito')
@@ -30,9 +19,3 @@ tipo_cliente = st.selectbox('Tipo de cliente', [0, 1, 2])
 
 if st.button('Predecir'):
     st.write('La probabilidad de que el cliente abandone el carrito es: X.XX')
-    """
-    datos_cliente = [detalles_producto, paginas_cambiadas, articulos_carrito, articulos_eliminados, 
-                      visualizaciones_carrito, inicios_pago, sesiones_usuario, paginas_visitadas, tipo_cliente]
-    probabilidad_abandono = predecir_abandono_carrito(datos_cliente)
-    st.write(f'La probabilidad de que el cliente abandone el carrito es: {probabilidad_abandono:.2f}')
-    """
