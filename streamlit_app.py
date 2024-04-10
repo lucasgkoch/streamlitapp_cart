08bf7b88a5163d656483a84447a794e1ab3e86bc
Lucas Koch <lucasgabrielkoch1997@gmail.com>
2024-04-10 14:10:37 -0300

    Se modifico el archivo streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,24 @@ import numpy as np
 # Interfaz de usuario de la aplicación
 st.title('Predicción de Abandono de Carrito')
 
-detalles_producto = st.checkbox('Detalles del producto vistos')
+column1, column2, column3 = st.columns(3)
+
+with column1:
+    detalles_producto = st.selectbox('Detalles del producto vistos', [0, 1])
+    articulos_eliminados = st.number_input('Cantidad de artículos eliminados del carrito', min_value=0, step=1)
+    sesiones_usuario = st.number_input('Cantidad de inicios de sesión del usuario', min_value=0, step=1)
+
+with column2:
+    paginas_cambiadas = st.number_input('Cantidad de veces que el cliente cambia de página', min_value=0, step=1)
+    visualizaciones_carrito = st.number_input('Número de visualizaciones del carrito', min_value=0, step=1)
+    paginas_visitadas = st.number_input('Cantidad de páginas visitadas', min_value=1, step=1)
+
+with column3:
+    articulos_carrito = st.number_input('Cantidad de artículos en el carrito', min_value=0, step=1)
+    inicios_pago = st.number_input('Número de veces que se inició el proceso de pago', min_value=0, step=1)
+    tipo_cliente = st.selectbox('Tipo de cliente', [0, 1, 2])
+
+detalles_producto = st.selectbox('Detalles del producto vistos', [0, 1])
 paginas_cambiadas = st.number_input('Cantidad de veces que el cliente cambia de página', min_value=0, step=1)
 articulos_carrito = st.number_input('Cantidad de artículos en el carrito', min_value=0, step=1)
 articulos_eliminados = st.number_input('Cantidad de artículos eliminados del carrito', min_value=0, step=1)
