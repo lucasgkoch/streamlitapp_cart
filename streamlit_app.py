e1dd4e7eaba4959ce8b9b3e18104663de6d45942
Lucas Koch <lucasgabrielkoch1997@gmail.com>
2024-04-10 14:02:32 -0300

    Se modifico el archivo streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ articulos_eliminados = st.number_input('Cantidad de artículos eliminados del ca
 visualizaciones_carrito = st.number_input('Número de visualizaciones del carrito', min_value=0, step=1)
 inicios_pago = st.number_input('Número de veces que se inició el proceso de pago', min_value=0, step=1)
 sesiones_usuario = st.number_input('Cantidad de inicios de sesión del usuario', min_value=0, step=1)
-paginas_visitadas = st.number_input('Cantidad de páginas visitadas', min_value=0, step=1)
+paginas_visitadas = st.number_input('Cantidad de páginas visitadas', min_value=1, step=1)
 tipo_cliente = st.selectbox('Tipo de cliente', [0, 1, 2])
 
 if st.button('Predecir'):
